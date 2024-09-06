36194309147a7a67457796f356a321bb3e39c318
sami342 <samuasami84@gmail.com>
2024-09-06 15:33:40 +0300

    deploy project

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.routs.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 dotenv.config();
 
@@ -17,7 +18,14 @@ mongoose
     console.log(err);
   });
 
+const __dirname=path.resolve();
+
 const app = expresss();
+app.use(expresss.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use(
   cors({
     origin: process.env.FRONT_END_URL,
